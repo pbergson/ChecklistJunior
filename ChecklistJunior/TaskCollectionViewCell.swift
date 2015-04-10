4aa120a76f63093c70a3978645bec392c6fe5d2d
Pamela Bergson <pamela@heliumfoot.com>
2015-04-09 20:06:07 -0500

    adds label, imageView and outlets

---
+++ ChecklistJunior/TaskCollectionViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  TaskCollectionViewCell.swift
+//  ChecklistJunior
+//
+//  Created by Pamela Bergson on 4/9/15.
+//  Copyright (c) 2015 com.mcmxcix. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class TaskCollectionViewCell : UICollectionViewCell {
+    
+    @IBOutlet var nameLabel : UILabel!
+    @IBOutlet var imageView : UIImageView!
+    
+}
